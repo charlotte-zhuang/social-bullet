aa35c759c5eb816e79374dd81e67f156624a9a26
charlotte-zhuang <charlotte.zhuang@sjsu.edu>
2021-01-08 22:36:43 -0800

    Changed tables to flex boxes

--- src/Components/template.jsx
+++ src/Components/template.jsx
@@ -1,6 +1,6 @@
-import '../CSS/template.css';
+import "../CSS/template.css";
 // import { AmplifySignOut } from '@aws-amplify/ui-react';
-import NavBtn from './navBtn.jsx';
+import NavBtn from "./navBtn.jsx";
 
 /**
  * A page with a title, navbar, and 3 column body.
@@ -12,30 +12,45 @@ import NavBtn from './navBtn.jsx';
  * @param {JSX.Element} props.bodyRight
  */
 function Template({ activePage, bodyLeft, bodyCenter, bodyRight }) {
-	return (
-		<div className="container-fluid">
-			<header className="row page-top-padding">
-				<div className="col-12">
-					<div className="justify-content-center">
-						<h1 className="advent-font text-center">Social Bullet</h1>
-					</div>
-					<nav className="d-flex justify-content-center">
-						<NavBtn title="Home" type="nav" selected={activePage === 'home'} url="/home" />
-						<NavBtn title="Discover" type="nav" selected={activePage === 'discover'} url="/discover" />
-						<NavBtn title="Journal" type="nav" selected={activePage === 'journal'} url="/journal" />
-						<NavBtn title="Settings" type="nav" selected={activePage === 'settings'} url="/settings" />
-						{/* <AmplifySignOut className="nav-btn" /> */}
-					</nav>
-				</div>
-			</header>
-			<div className="left-panel"></div>
-			<div className="row">
-				<div className="col-2">{bodyLeft}</div>
-				<div className="col-8">{bodyCenter}</div>
-				<div className="col-2">{bodyRight}</div>
-			</div>
-		</div>
-	);
+  return (
+    <>
+      <header className='page-top-padding'>
+        <h1 className='advent-font text-center'>Social Bullet</h1>
+        <nav className='d-flex justify-content-center'>
+          <NavBtn
+            title='Home'
+            type='nav'
+            selected={activePage === "home"}
+            url='/home'
+          />
+          <NavBtn
+            title='Discover'
+            type='nav'
+            selected={activePage === "discover"}
+            url='/discover'
+          />
+          <NavBtn
+            title='Journal'
+            type='nav'
+            selected={activePage === "journal"}
+            url='/journal'
+          />
+          <NavBtn
+            title='Settings'
+            type='nav'
+            selected={activePage === "settings"}
+            url='/settings'
+          />
+          {/* <AmplifySignOut className="nav-btn" /> */}
+        </nav>
+      </header>
+      <div className='d-flex justify-content-around'>
+        <div className='body-left'>{bodyLeft}</div>
+        <div className='body-center'>{bodyCenter}</div>
+        <div className='body-right'>{bodyRight}</div>
+      </div>
+    </>
+  );
 }
 
 export default Template;
