29a0474ee64ad1d0cb2b50689fa7b8d44aebd432
aprilchao <aprilemeraldchao@gmail.com>
2021-01-10 08:46:32 -0800

    added add friend ui

--- src/Components/friendPanel.jsx
+++ src/Components/friendPanel.jsx
@@ -2,15 +2,13 @@ import ListView from './listView';
 import PanelBtn from './panelBtn';
 
 function FriendPanel({ friends }) {
+	var insertQuery = (username) => {
+		console.log(username);
+	};
 	return (
-		<div className="d-flex flex-column align-items-center full-height">
+		<div className="d-flex flex-column align-items-center full-height full-width">
 			<ListView listItems={friends} title="Friends" size="md" />
-			<PanelBtn
-				title="Find Friends"
-				clicked={() => {
-					console.log('clicked Find Friends');
-				}}
-			/>
+			<PanelBtn title="Add Friend" input clicked={insertQuery} />
 		</div>
 	);
 }
--- src/Components/panelBtn.jsx
+++ src/Components/panelBtn.jsx
@@ -1,10 +1,32 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import '../CSS/btns.css';
+import { useState } from 'react';
 
-function PanelBtn({ title, clicked }) {
+function PanelBtn({ title, clicked, input }) {
+	const [username, changeUsername] = useState('');
+
 	return (
 		<div className="panel-btn-container mt-auto">
-			<a className="panel-btn" onClick={() => clicked(console.log('clicked ' + { title }))}>
+			{input ? (
+				<input
+					type="text"
+					onChange={(e) => {
+						changeUsername(e.target.value);
+					}}
+					className="form-control form-txt-input panel-input mb-2"
+					placeholder="Input a username"
+					value={username}
+				/>
+			) : (
+				<></>
+			)}
+			<a
+				className="panel-btn"
+				onClick={() => {
+					clicked(username);
+					if (input) changeUsername('');
+				}}
+			>
 				{title}
 			</a>
 		</div>
