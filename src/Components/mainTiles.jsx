1810c33b0d14875dafef0ec8873cb4c78e85b4b1
aprilchao <aprilemeraldchao@gmail.com>
2021-01-10 00:31:40 -0800

    finished credits page

--- src/Components/mainTiles.jsx
+++ src/Components/mainTiles.jsx
@@ -1,10 +1,12 @@
 import ProjectTile from './projectTile';
 import FriendTile from './friendTile';
+import ContrTile from './contrTile';
 
 function MainTiles({ type, tileItems }) {
 	const processTiles = () => {
 		if (type === 'project') return tileItems.map((item, key) => <ProjectTile project={item} key={key} />);
 		if (type === 'friend') return tileItems.map((item, key) => <FriendTile friend={item} key={key} />);
+		if (type === 'contributor') return tileItems.map((item, key) => <ContrTile contributor={item} key={key} />);
 	};
 	return <div className="d-flex flex-wrap justify-content-around align-items-center full-width">{processTiles()}</div>;
 }
