4fa0beeaa1094d576ae9252a4f684a12065de133
aprilchao <aprilemeraldchao@gmail.com>
2021-01-10 10:13:11 -0800

    dummy stuff

--- src/Pages/Home/HomeEntriesPage.jsx
+++ src/Pages/Home/HomeEntriesPage.jsx
@@ -1,306 +1,87 @@
-import Template from "../../Components/template.jsx";
-import Submenu from "../../Components/submenu.jsx";
-import FriendPanel from "../../Components/friendPanel.jsx";
-import MainList from "../../Components/mainList.jsx";
+import Template from '../../Components/template.jsx';
+import Submenu from '../../Components/submenu.jsx';
+import FriendPanel from '../../Components/friendPanel.jsx';
+import MainList from '../../Components/mainList.jsx';
 
 function HomeEntriesPage(props) {
-  const menuItems = [
-    {
-      isRoute: true,
-      title: "Entries",
-      selected: true,
-      url: "/home",
-    },
-    {
-      isRoute: true,
-      title: "Projects",
-      selected: false,
-      url: "/home-projects",
-    },
-  ];
+	const menuItems = [
+		{
+			isRoute: true,
+			title: 'Entries',
+			selected: true,
+			url: '/home',
+		},
+		{
+			isRoute: true,
+			title: 'Projects',
+			selected: false,
+			url: '/home-projects',
+		},
+	];
 
-  const friends = [
-    {
-      name: "Mal Jones",
-      url: "/profile",
-    },
-    {
-      name: "Mal Jones",
-      url: "/profile",
-    },
-    {
-      name: "Mal Jones",
-      url: "/profile",
-    },
-  ];
+	const friends = [
+		{
+			name: 'April Chao',
+			url: '/profile/chaos21',
+		},
+		{
+			name: 'Charlotte Zhuang',
+			url: '/profile/bunbun',
+		},
+		{
+			name: 'Ethan Soo',
+			url: '/profile/wingspear',
+		},
+	];
 
-  const entries = [
-    {
-      user: {
-        url: "/profile",
+	const listItems =
+		props.user === null
+			? []
+			: props.user.entryFeed.map((entry) => {
+					props.user.url = '/profile/' + props.user.username;
+					entry.taskList = [
+						{
+							text: (
+								<p className="advent-font">
+									Added <i>January Cooking</i> Project <b>[at 7:00 am]</b>
+								</p>
+							),
 
-        name: "Mal Jones",
-      },
-      entry: {
-        date: "Jan 2, 2021",
-        text:
-          "Today was lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sodales libero sit amet lacus varius, id tincidunt risus posuere. Mauris velit sem, pulvinar id hendrerit et, malesuada sed quam.",
-      },
-      taskList: [
-        {
-          text: (
-            <p className="advent-font">
-              Added <i>January Cooking</i> Project <b>[at 7:00 am]</b>
-            </p>
-          ),
+							url: '/project',
+						},
+						{
+							text: (
+								<p className="advent-font">
+									Completed Yoga from <i>2021 Fitness</i> Project <b>[at 7:28 am]</b>
+								</p>
+							),
 
-          url: "/project",
-        },
-        {
-          text: (
-            <p className="advent-font">
-              Completed Yoga from <i>2021 Fitness</i> Project{" "}
-              <b>[at 7:28 am]</b>
-            </p>
-          ),
+							url: '/project',
+						},
+						{
+							text: (
+								<p className="advent-font">
+									Challenged Friend 1 to <i>January Cooking</i> Project <b>[at 8:00 am]</b>
+								</p>
+							),
 
-          url: "/project",
-        },
-        {
-          text: (
-            <p className="advent-font">
-              Challenged Friend 1 to <i>January Cooking</i> Project{" "}
-              <b>[at 8:00 am]</b>
-            </p>
-          ),
-
-          url: "/project",
-        },
-      ],
-    },
-    {
-      user: {
-        url: "/profile",
-
-        name: "Mal Jones",
-      },
-      entry: {
-        date: "Jan 2, 2021",
-        text:
-          "Today was lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sodales libero sit amet lacus varius, id tincidunt risus posuere. Mauris velit sem, pulvinar id hendrerit et, malesuada sed quam.",
-      },
-      taskList: [
-        {
-          text: (
-            <p className="advent-font">
-              Added <i>January Cooking</i> Project <b>[at 7:00 am]</b>
-            </p>
-          ),
-
-          url: "/project",
-        },
-        {
-          text: (
-            <p className="advent-font">
-              Completed Yoga from <i>2021 Fitness</i> Project{" "}
-              <b>[at 7:28 am]</b>
-            </p>
-          ),
-
-          url: "/project",
-        },
-        {
-          text: (
-            <p className="advent-font">
-              Challenged Friend 1 to <i>January Cooking</i> Project{" "}
-              <b>[at 8:00 am]</b>
-            </p>
-          ),
-
-          url: "/project",
-        },
-      ],
-    },
-    {
-      user: {
-        url: "/profile",
-
-        name: "Mal Jones",
-      },
-      entry: {
-        date: "Jan 2, 2021",
-        text:
-          "Today was lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sodales libero sit amet lacus varius, id tincidunt risus posuere. Mauris velit sem, pulvinar id hendrerit et, malesuada sed quam.",
-      },
-      taskList: [
-        {
-          text: (
-            <p className="advent-font">
-              Added <i>January Cooking</i> Project <b>[at 7:00 am]</b>
-            </p>
-          ),
-
-          url: "/project",
-        },
-        {
-          text: (
-            <p className="advent-font">
-              Completed Yoga from <i>2021 Fitness</i> Project{" "}
-              <b>[at 7:28 am]</b>
-            </p>
-          ),
-
-          url: "/project",
-        },
-        {
-          text: (
-            <p className="advent-font">
-              Challenged Friend 1 to <i>January Cooking</i> Project{" "}
-              <b>[at 8:00 am]</b>
-            </p>
-          ),
-
-          url: "/project",
-        },
-      ],
-    },
-    {
-      user: {
-        url: "/profile",
-
-        name: "Mal Jones",
-      },
-      entry: {
-        date: "Jan 2, 2021",
-        text:
-          "Today was lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sodales libero sit amet lacus varius, id tincidunt risus posuere. Mauris velit sem, pulvinar id hendrerit et, malesuada sed quam.",
-      },
-      taskList: [
-        {
-          text: (
-            <p className="advent-font">
-              Added <i>January Cooking</i> Project <b>[at 7:00 am]</b>
-            </p>
-          ),
-
-          url: "/project",
-        },
-        {
-          text: (
-            <p className="advent-font">
-              Completed Yoga from <i>2021 Fitness</i> Project{" "}
-              <b>[at 7:28 am]</b>
-            </p>
-          ),
-
-          url: "/project",
-        },
-        {
-          text: (
-            <p className="advent-font">
-              Challenged Friend 1 to <i>January Cooking</i> Project{" "}
-              <b>[at 8:00 am]</b>
-            </p>
-          ),
-
-          url: "/project",
-        },
-      ],
-    },
-    {
-      user: {
-        url: "/profile",
-
-        name: "Mal Jones",
-      },
-      entry: {
-        date: "Jan 2, 2021",
-        text:
-          "Today was lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sodales libero sit amet lacus varius, id tincidunt risus posuere. Mauris velit sem, pulvinar id hendrerit et, malesuada sed quam.",
-      },
-      taskList: [
-        {
-          text: (
-            <p className="advent-font">
-              Added <i>January Cooking</i> Project <b>[at 7:00 am]</b>
-            </p>
-          ),
-
-          url: "/project",
-        },
-        {
-          text: (
-            <p className="advent-font">
-              Completed Yoga from <i>2021 Fitness</i> Project{" "}
-              <b>[at 7:28 am]</b>
-            </p>
-          ),
-
-          url: "/project",
-        },
-        {
-          text: (
-            <p className="advent-font">
-              Challenged Friend 1 to <i>January Cooking</i> Project{" "}
-              <b>[at 8:00 am]</b>
-            </p>
-          ),
-
-          url: "/project",
-        },
-      ],
-    },
-  ];
-  const listItems =
-    props.user === null
-      ? []
-      : props.user.entryFeed.map((entry) => {
-          props.user.url = "/profile/" + props.user.username;
-          entry.taskList = [
-            {
-              text: (
-                <p className="advent-font">
-                  Added <i>January Cooking</i> Project <b>[at 7:00 am]</b>
-                </p>
-              ),
-
-              url: "/project",
-            },
-            {
-              text: (
-                <p className="advent-font">
-                  Completed Yoga from <i>2021 Fitness</i> Project{" "}
-                  <b>[at 7:28 am]</b>
-                </p>
-              ),
-
-              url: "/project",
-            },
-            {
-              text: (
-                <p className="advent-font">
-                  Challenged Friend 1 to <i>January Cooking</i> Project{" "}
-                  <b>[at 8:00 am]</b>
-                </p>
-              ),
-
-              url: "/project",
-            },
-          ];
-          return {
-            user: props.user,
-            entry,
-          };
-        });
-  console.log(listItems);
-  return (
-    <Template
-      activePage="home"
-      bodyLeft={<FriendPanel friends={friends} user={props.user} />}
-      bodyCenter={<MainList type="entry" listItems={listItems} />}
-      bodyRight={<Submenu menuItems={menuItems} />}
-    />
-  );
+							url: '/project',
+						},
+					];
+					return {
+						user: props.user,
+						entry,
+					};
+			  });
+	console.log(listItems);
+	return (
+		<Template
+			activePage="home"
+			bodyLeft={<FriendPanel friends={friends} user={props.user} />}
+			bodyCenter={<MainList type="entry" listItems={listItems} />}
+			bodyRight={<Submenu menuItems={menuItems} />}
+		/>
+	);
 }
 
 export default HomeEntriesPage;
