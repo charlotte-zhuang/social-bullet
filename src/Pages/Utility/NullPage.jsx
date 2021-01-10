9d3545dd5b8068882775f32a26b9b6eba4051767
aprilchao <aprilemeraldchao@gmail.com>
2021-01-09 21:01:13 -0800

    finished rd of null page

--- src/Pages/Utility/NullPage.jsx
+++ src/Pages/Utility/NullPage.jsx
@@ -1,7 +1,19 @@
 import Template from '../../Components/template.jsx';
 
 function NullPage() {
-	return <Template activePage="" bodyLeft={<></>} bodyCenter={<>Sorry! This Page doesn't seem to exist yet.</>} bodyRight={<></>} />;
+	return (
+		<Template
+			activePage=""
+			bodyLeft={<></>}
+			bodyCenter={
+				<div className="d-flex flex-column p-5 text-center null-container">
+					<p className="advent-font null-msg-big">Sorry!</p>
+					<p className="advent-font null-msg">This Page doesn't seem to exist yet.</p>
+				</div>
+			}
+			bodyRight={<></>}
+		/>
+	);
 }
 
 export default NullPage;
