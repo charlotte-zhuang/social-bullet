7972c96133ce14bee846a50020bade4423cc89e9
aprilchao <aprilemeraldchao@gmail.com>
2021-01-10 06:35:10 -0800

    fixed date picker not showing year

--- src/Components/datePick.jsx
+++ src/Components/datePick.jsx
@@ -1,13 +1,24 @@
 import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { useState } from 'react';
 import MomentUtils from '@date-io/moment';
-import '../CSS/inputs.css'
+import '../CSS/inputs.css';
 
 function DatePick() {
 	const [selectedDate, handleDateChange] = useState(new Date());
 	return (
 		<MuiPickersUtilsProvider utils={MomentUtils}>
-			<DatePicker value={selectedDate} onChange={handleDateChange} className="date-picker-custom"/>
+			<DatePicker
+				labelFunc={() => {
+					return new Date(selectedDate).toLocaleString('default', {
+						month: 'short',
+						day: 'numeric',
+						year: 'numeric',
+					});
+				}}
+				value={selectedDate}
+				onChange={handleDateChange}
+				className="date-picker-custom"
+			/>
 		</MuiPickersUtilsProvider>
 	);
 }
